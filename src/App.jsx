73db7243c8fb31490a46f730a1b89f49b561f21b
ravi-shanker239 <135230864+ravi-shanker239@users.noreply.github.com>
2023-12-06 23:20:09 +0500

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import Router from "./router/router"
-
+// import './App.css';
+// import './src/index.css';
 function App() {
   return (
     <>
       <BrowserRouter>
         <Router />
       </BrowserRouter>
+      
     </>
   )
 }
